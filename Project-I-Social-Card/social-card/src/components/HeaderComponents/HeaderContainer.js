f888159f2eaf17792ffa064442090fdf31f87b6e
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-06-03 15:55:38 +0100

    linking import and exports with image thumbnail

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+// import HeaderContent from './HeaderContent';
+
+function HeaderContainer() {
+  return (
+    <div className="HeaderContainer">
+      <ImageThumbnail/>
+      <HeaderTitle/>
+      {/* <HeaderContent/> */}
+    </div>
+  );
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>HELLO</h1>
+//       <BasicButton text='Click Me!'/>
+//       <SectionSome />
+//       <GoogleIt />
+//     </div>
+//   );
+// }
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle () {
+  return (
+    <div>
+      <h1>Lambda School</h1>
+      <h2>@LambdaSchool</h2>
+      <p>3rd June</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
+
