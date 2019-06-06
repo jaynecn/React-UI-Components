fbc8d4646ab92b45fef4f81a97420cc4c36e5236
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-06-06 22:27:21 +0100

    starting on calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Button.css';
+
+class NumberButton extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <button className={props.buttonStyle}>
+      {props.text}
+      </button>
+    )
+  }
+
+}
+
+
+export default NumberButton;
